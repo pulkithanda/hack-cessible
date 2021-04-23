10c570d12cc56c034c55372664b43c0a66b416dd
aayanagarwal05@gmail.com <49789627+aayan05@users.noreply.github.com>
2021-04-23 15:47:58 +0530

    routes

--- covid19/routes.py
+++ covid19/routes.py
@@ -7,3 +7,18 @@ from covid19 import app
 @app.route("/")
 def home():
     return render_template("home.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="About")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html", title="Login")
+
+
+@app.route("/register")
+def register():
+    return render_template('register.html', title="Register")
