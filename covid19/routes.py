c062b7e13be1fb6128ec62e98f938b7b41aa1fdc
Matthew Dion <mkdion@uci.edu>
2021-04-23 18:41:35 -0700

    login branch (untested)

--- covid19/routes.py
+++ covid19/routes.py
@@ -1,7 +1,9 @@
 from flask import Flask
+import flask_login
 from flask.templating import render_template
-from covid19 import app
-from covid19.forms import RegistrationForm
+from covid19 import app, login_manager
+from covid19.forms import RegistrationForm, LoginForm
+from covid19.models import User
 
 
 @app.route("/home")
@@ -15,12 +17,25 @@ def about():
     return render_template("about.html", title="About")
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template("login.html", title="Login")
-
+    form = LoginForm()
+    if form.validate_on_submit():
+        login_user(user, remember=remember)
+        '''return flask.redirect(flask.url_for('post'), title="Create Post")'''
+    return render_template("login.html", title="Login", form=form)
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     return render_template('register.html', title="Register", form=form)
+
+
+'''
+from flask_login import login_required, current_user
+
+@app.route("/post", methods=['GET, POST'])
+@login_required
+def postCreate():
+    return render_template('postCreate.html', title="Create Post", name=current_user)
+'''
